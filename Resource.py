80b2c35310cebaf590005fcb8a82037e05d89f57
yumueat <1471063903@qq.com>
2023-02-26 21:15:01 +0800

    Update Resource.py

--- Resource.py
+++ Resource.py
@@ -12,9 +12,11 @@ class Resource:
     name = None
     block_process_queue = queue.Queue()
     number = None
-    def __init__(self,name,number):
+
+    def __init__(self, name, number):
         self.number = number
         self.name = name
+
 
 class Resource_allocation:
     resource_allocation_log = []
@@ -22,12 +24,14 @@ class Resource_allocation:
     available = []
     safety_sequence = []
 
+
 class Resource_dictionary:
     table = {
-        "A":0,
-        "B":1,
-        "C":2,
+        "A": 0,
+        "B": 1,
+        "C": 2,
     }
+
 
 class Event:
     event_dic = {
